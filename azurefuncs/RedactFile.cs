5baefe852a63e0ed1e241f70f0094210842e5bde
steve <srosam@gmail.com>
2020-02-12 09:31:37 +0000

    swagger yaml changes

--- azurefuncs/RedactFile.cs
+++ azurefuncs/RedactFile.cs
@@ -1,12 +1,9 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Glasswall.Redact.Api
 {
@@ -23,9 +20,9 @@ namespace Glasswall.Redact.Api
                 return new BadRequestObjectResult("You must specify a file and an xml");
             
             var fileArg = req.Form.Files[0];
-            req.Form.TryGetValue("RedactJson", out var jsonArg);
+            req.Form.TryGetValue("redactJson", out var jsonArg);
 
-            return new OkObjectResult("Received a file to redact stuff in: " + fileArg.FileName + " and an JSON: " + jsonArg);
+            return new OkObjectResult("Received a file to redact stuff in: " + fileArg.FileName + " and JSON: " + jsonArg);
         }
     }
 }
